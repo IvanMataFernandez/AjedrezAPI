cca885625fbc3b953a83e8afd2adab83787d0608
Elsab <Elsab@LAPTOP-AENUKPPD>
2023-11-18 20:43:04 +0100

    Añadir compatibilidad con Linux

--- Ajedrez/src/vista/Pieza.java
+++ Ajedrez/src/vista/Pieza.java
@@ -1,12 +1,15 @@
 package vista;
-import javax.swing.*;
+import java.io.File;
 
+import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class Pieza extends JLabel {
 	
-	private final String RUTA_IMAGENES = ".\\piezaspng\\";
-		
+//	private final String RUTA_IMAGENES = ".\\piezaspng\\";
+	private final String RUTA_IMAGENES = "."+File.separator+"piezaspng"+File.separator;
+
+	
 	public Pieza (int pTipoDePieza, boolean pBlanco) {
 		// Pre: Tipo de pieza: 0 -> Peon, 1 -> Caballo, 2 -> Alfil, 3 -> Torre, 4 -> Reina, 5 -> Rey
 
